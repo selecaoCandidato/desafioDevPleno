5071013bdf737b270e379b3c683165ffb778a35f
arthurramires <arthurramires1802@outlook.com>
2020-07-03 14:47:57 -0400

    Criação do pacote back-end

---
+++ backend/apifinances/apifinances/src/main/java/com/desafio/apifinances/ApifinancesApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.apifinances;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApifinancesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApifinancesApplication.class, args);
+	}
+
+}
---
+++ backend/apifinances/apifinances/src/test/java/com/desafio/apifinances/ApifinancesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.apifinances;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApifinancesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
